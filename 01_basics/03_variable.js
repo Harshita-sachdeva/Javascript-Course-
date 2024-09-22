4f56601807b1b6d4d19539e5cc87d2f83dd9d14c
Harshita sachdeva <harshitasachdeva04@gmail.com>
2024-09-22 18:33:55 +0000

    update

---
+++ 01_basics/03_variable.js
@@ -0,0 +1,44 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let temp=null
+// console.log(typeof(temp)) // type when null
+// let temp1=Number(temp)
+// console.log(temp1)        //type of null=>number
+
+
+
+// ************************************
+/*conversion to number */
+//"33" => 33
+//"33abc"=> Nan
+//true=>1 ; false=> 0
+//null=>0
+// ************************************
+
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(typeof IsloggedIn);
+// console.log(booleanIsloggedIn);
+
+
+// ************************************
+// 1=>true ; 0=>false
+// ""=>false
+// "harshita"=>true
+// ************************************
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
